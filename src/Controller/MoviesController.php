b197b519561d56e870a8ecacea46bc43c1836193
siw-ss <siwarsoula@gmail.com>
2024-01-31 16:35:35 +0100

    Upload image works+add item to DB

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -5,16 +5,21 @@ namespace App\Controller;
 use App\Entity\Movie;
 use App\Form\MovieFormType;
 use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+    private $em;
     private $movieRepository;
-    public function __construct(MovieRepository $movieRepository)
+    public function __construct(MovieRepository $movieRepository, EntityManagerInterface $em)
     {
         $this->movieRepository = $movieRepository;
+        $this->em = $em;
     }
 
     #[Route('/', methods:['GET'], name: 'movies_app')]
@@ -26,10 +31,35 @@ class MoviesController extends AbstractController
     }
 
     #[Route('/movies/create', name: 'movies_create')]
-    public function create(): Response
+    public function create(Request $request): Response
     {
         $movie = new Movie();
         $form = $this->createForm(MovieFormType::class, $movie);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newMovie = $form->getData();
+
+            $imagePath = $form->get('imagePath')->getData();
+            if ($imagePath) {
+                $newFileName = uniqid(). '.' .$imagePath->guessExtension();
+
+                try {
+                    $imagePath->move(
+                        $this->getParameter('kernel.project_dir') . '/public/uploads',
+                        $newFileName
+                    );
+                }catch(FileException $e){
+                    return new Response($e->getMessage());
+                }
+
+                $newMovie->setImagePath('/uploads/' . $newFileName);
+            }
+            $this->em->persist($newMovie);
+            $this->em->flush();
+
+            return $this->redirectToRoute('movies_app');
+        }
 
         return $this->render('movies/create.html.twig',[
             'form' => $form->createView()
