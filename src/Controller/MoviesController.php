3042359433cb88288721d614e888bd9200ef25f5
siw-ss <siwarsoula@gmail.com>
2024-01-31 15:00:55 +0100

    Show item page by id added

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Movie;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,14 +17,23 @@ class MoviesController extends AbstractController
         $this->em = $em;
     }
 
-    #[Route('/', name: 'app_movies')]
+    #[Route('/', methods:['GET'], name: 'movies_app')]
     public function index(): Response
     {
         $repository = $this->em->getRepository(Movie::class);
-        $movies = $repository->findAll();
 
         return $this->render('movies/index.html.twig',[
-            'movies' => $movies,
+            'movies' => $repository->findAll()
+        ]);
+    }
+
+    #[Route('/movies/{id}', methods:['GET'], name: 'movies_show')]
+    public function show($id): Response
+    {
+        $repository = $this->em->getRepository(Movie::class);
+
+        return $this->render('movies/show.html.twig',[
+            'movie' => $repository->find($id)
         ]);
     }
 }
