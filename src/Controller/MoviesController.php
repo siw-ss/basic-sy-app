1d85eed8ad73d7e2833dd4adb2a2959b7697abc4
Siwar Soula <siwarsoula@gmail.com>
2024-02-19 15:10:50 +0100

    CRUD APIs; create + find + update + delete

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -12,6 +12,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\Context\Normalizer\ObjectNormalizerContextBuilder;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -55,9 +57,16 @@ class MoviesController extends AbstractController
             true
         );
     }
+    
+/////// Return a JSON response
+    #[Route('/test', methods:['GET'], name: 'movies_api')]
+    public function hello(): JsonResponse
+    {
+        return $this->json(['message' => 'Hello from Symfony API!']);
+    }
 
     //GET ALL MOVIES API - JSON FORMAT - NO ACTORS
-    #[Route('/moviesonly', methods:['GET'], name: 'movies_only')]
+    #[Route('/api/movies', methods:['GET'], name: 'movies_only')]
     public function getonlymovies(): JsonResponse
     {
         $movies = $this->movieRepository->findAll();
@@ -75,13 +84,35 @@ class MoviesController extends AbstractController
         );
     }
 
+//////Create new movie api NO template
+    #[Route('/api/create', methods:['POST'], name:'movie_create')]
+    public function add(Request $request): JsonResponse
+    {
+        $movie= $this->serializer->deserialize(
+            $request->getContent(),
+            Movie::class, 'json');
+        $this->em->persist($movie);
+        $this->em->flush();
+
+        $jsonMovie= $this->serializer->serialize(
+            $movie,'json',['groups'=>'getMovies']); //context
+
+        return new JsonResponse(
+            $jsonMovie,
+            Response::HTTP_CREATED,
+            ['Content-Type' => 'application/json'],
+            true
+        );
+    }
+
+    
     //CREATE NEW MOVIE API
     #[Route('/movies/create', name: 'movies_create')]
     public function create(Request $request): Response
     {
         $movie = new Movie();
         $form = $this->createForm(MovieFormType::class, $movie);
-
+        
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $newMovie = $form->getData();
@@ -98,20 +129,20 @@ class MoviesController extends AbstractController
                 }catch(FileException $e){
                     return new Response($e->getMessage());
                 }
-
+                
                 $newMovie->setImagePath('/uploads/' . $newFileName);
             }
             $this->em->persist($newMovie);
             $this->em->flush();
-
+            
             return $this->redirectToRoute('movies_app');
         }
-
+        
         return $this->render('movies/create.html.twig',[
             'form' => $form->createView()
         ]);
     }
-    
+
     //GET SPECIFIC MOVIE API
     #[Route('/movies/{id}', methods:['GET'], name: 'movies_show')]
     public function show($id): Response
@@ -119,6 +150,49 @@ class MoviesController extends AbstractController
         return $this->render('movies/show.html.twig',[
             'movie' => $this->movieRepository->find($id)
         ]);
+    }
+
+///////GET SPECIFIC MOVIE API NO TEMPLATE
+    #[Route('/api/find/{id}', methods:['GET'], name: 'movies_show')]
+    public function showMovie($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+        if (!$movie) {
+            return $this->json('No movie found for id ' . $id, 404);
+        }
+        $data =  [
+            'title' => $movie->getTitle(),
+            'releaseYear' => $movie->getReleaseYear(),
+            'description' => $movie->getDescription(),
+            'imagePath' => $movie->getImagePath(),
+        ];
+        return $this->json($data);
+    }
+
+/////////Update movie api NO template
+    #[Route('/api/update/{id}', name:"movie_update", methods:['PUT'])]
+    public function updateMovie($id, Request $request): JsonResponse 
+    {
+        $movie = $this->movieRepository->findOneBy(['id' => $id]);
+        if (!$movie) {
+            return $this->json('No movie found for id ' . $id, 404);
+        }
+        $data= json_decode($request->getContent(),true);
+
+        empty($data['title'])? true : $movie->setTitle($data['title']);
+        empty($data['releaseYear'])? true : $movie->setReleaseYear($data['releaseYear']);
+        empty($data['description'])? true : $movie->setDescription($data['description']);
+        empty($data['imagePath'])? true : $movie->setImagePath($data['imagePath']);
+        $this->em->persist($movie);
+        $this->em->flush();
+            
+        $data =  [
+            'title' => $movie->getTitle(),
+            'releaseYear' => $movie->getReleaseYear(),
+            'description' => $movie->getDescription(),
+            'imagePath' => $movie->getImagePath(),
+        ];
+        return $this->json($data);
     }
 
     //UPDATE A SPECIFIC MOVIE
@@ -167,7 +241,20 @@ class MoviesController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
-    
+
+//////DELETE A MOVIE API NO TEMPLATE
+    #[Route('/api/delete/{id}', methods:['GET','DELETE'], name:'movie_delete')]
+    public function deleteMovie($id): JsonResponse
+    {
+        $movie = $this->movieRepository->find($id);
+        if (!$movie) {
+            return $this->json('No movie found for id ' . $id, 404);
+        }
+        $this->em->remove($movie);
+        $this->em->flush();
+
+        return $this->json('Deleted a project successfully with id ' . $id);
+    }
     //DELETE A MOVIE API
     #[Route('/movies/delete/{id}', methods:['GET','DELETE'], name:'movies_delete')]
     public function delete($id): Response
